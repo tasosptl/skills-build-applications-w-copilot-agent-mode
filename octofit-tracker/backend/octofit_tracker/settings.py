3e9983b5dbcbaaea8bf623bfd18a2dde08814329
tasosptl <53862819+tasosptl@users.noreply.github.com>
2025-04-24 15:35:23 +0000

    Adding REST API support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-25rsll_s*6ml5lv4l$51z6x!y5u_k!11f!hf^1&%q!$syk=ja3
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'psychic-barnacle-r64g64rgwg43p5wx-8000.app.github.dev']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://psychic-barnacle-r64g64rgwg43p5wx-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
